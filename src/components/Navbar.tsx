7d8a5828c40764db12c51be03d810704085a14cb
Joshua Carroll <carrolljosh1976@gmail.com>
2022-12-11 13:07:06 -0500

    started pet project, added tailwind css and navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <h1 className='text-2xl font-bold'>Friends of Feral Felines</h1>
+    </div>
+  )
+}
+
+export default Navbar
