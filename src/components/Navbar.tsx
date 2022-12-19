3ac3df1ffbf475a2da7a99fec38584b68540406e
Joshua Carroll <carrolljosh1976@gmail.com>
2022-12-18 21:43:07 -0500

    Added react icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React from 'react';
+import {BsTwitter} from "react-icons/bs";
+import {FaFacebookF} from "react-icons/fa";
+import {ImYoutube2} from "react-icons/im";
+import {AiFillInstagram} from "react-icons/ai";
 
 function Navbar() {
   return (
     <header>
-        <h1 className='text-2xl font-bold'>Friends of Feral Felines</h1>
+        <div>
+          <h1 className='text-2xl font-bold'>Friends of Feral Felines</h1>
 
-        <ul>
-          <li>About</li>
-          <li>Press</li>
-        </ul>
+          <ul>
+           <li>About</li>
+            <li>Press</li>
+          </ul>
+          <BsTwitter />
+          <FaFacebookF />
+          <ImYoutube2 />
+          <AiFillInstagram />
+        </div>
     </header>
   )
 }
