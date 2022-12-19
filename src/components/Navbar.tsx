14f91f90383a82803e8ba3835cbc708a3b7f9f51
Joshua Carroll <carrolljosh1976@gmail.com>
2022-12-18 21:28:20 -0500

    Added unordered list and installed hero icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>
+    <header>
         <h1 className='text-2xl font-bold'>Friends of Feral Felines</h1>
-    </div>
+
+        <ul>
+          <li>About</li>
+          <li>Press</li>
+        </ul>
+    </header>
   )
 }
 
