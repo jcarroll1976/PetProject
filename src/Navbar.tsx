a1cd2db519db7dc15d424ab94615c19755f6fb98
Joshua Carroll <carrolljosh1976@gmail.com>
2022-12-20 23:23:17 -0500

    Added to Navbar component

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div></div>
+    <div>
+        <p>Cat Care</p>
+        <p>Resources</p>
+        <p>Our Work</p>
+        <p>Stories</p>
+        <p>Ways to Give</p>
+        <p>Shop</p>
+    </div>
   )
 }
 
