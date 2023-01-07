83c6c9acaf86a1ea4356d82a631bcc9850f21766
Joshua Carroll <carrolljosh1976@gmail.com>
2023-01-06 22:48:13 -0500

    added some styling to navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,13 +3,13 @@ import catPhoto from "../ursa-bavcar-nNmWt0roQ-E-unsplash.jpg"
 
 function Navbar() {
   return (
-    <div className='flex flex-col space-y-4 justify-center items-center'>
+    <div className='flex flex-col max-w-[1640px] mx-auto max-h-[1200px] space-y-4 justify-center items-center'>
         <div className='flex space-x-8'>
         <p>Cat Care</p>
         <p>Feeding Locations</p>
         <p>Resources</p>
         </div>
-        <img className="h-96" src={catPhoto} alt='' />
+        <img className="w-full object-cover max-h-[1000px]" src={catPhoto} alt='' />
         <h2 className='text-2xl'>Help Cats During Colder Months</h2>
         <h3 className='text-xl'>How to Build An Outdoor Shelter</h3>
     </div>
