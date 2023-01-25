a4e3f148704e9acbc638b0a633fd68557f5f0dc6
Joshua Carroll <carrolljosh1976@gmail.com>
2023-01-24 20:07:25 -0500

    Added some styling to footer

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -2,7 +2,27 @@ import React from 'react'
 
 function CTA() {
   return (
-    <div>CTA</div>
+    <div>
+        <div>
+            <h4>Our Work</h4>
+            <ul>
+                <li>Animal Shelters</li>
+                <li>Anti-Cruelty</li>
+                <li>Cats and the Law</li>
+                <li>Spay and Neuter</li>
+                <li>Trap-Neuter-Return(TNR)</li>
+            </ul>
+        </div>
+        <div>
+            <h4>Resources</h4>
+            <ul>
+                <li>Cat Care Center</li>
+                <li>Our Network</li>
+                <li>TNR Resources</li>
+                <li>Veterinary Resource Center</li>
+            </ul>
+        </div>
+    </div>
   )
 }
 
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,9 +3,9 @@ import { FaCat } from 'react-icons/fa'
 
 function Footer() {
   return (
-    <div>
-      <FaCat /><h1 className='flex text-2xl font-bold space-x-4 text-blue-400'>Friends of Feral Felines</h1>
-      <ul>
+    <div >
+      <h1 className='flex text-2xl font-bold space-x-4 text-blue-400'><FaCat />Friends of Feral Felines</h1>
+      <ul className='flex flex-col md:flex justify-between'>
         <li>About Friends of Feral Felines</li>
         <li>Contact</li>
         <li>Frequently Asked Questions</li>
